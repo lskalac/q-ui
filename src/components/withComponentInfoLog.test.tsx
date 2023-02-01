b79f4843bf7d670b7dada44f30435550bd778a20
Lorena Škalac <lorena.skalac@softwaresauna.com>
2023-02-02 00:08:55 +0100

    - tests for Page and withComponentInfoLog

---
+++ src/components/withComponentInfoLog.test.tsx
@@ -0,0 +1,26 @@
+import {describe, expect, it, vi} from 'vitest';
+import {render, screen} from '@testing-library/react';
+import { withComponentInfoLog } from './withComponentInfoLog';
+
+const TestComponent = () => {
+    return <p>Test</p>
+}
+
+const Component = withComponentInfoLog(TestComponent);
+
+describe('withComponentInfoLog', () => {
+	it('should display component', () => {
+        render(<Component />);
+
+		expect(screen.getByText('Test')).toBeDefined();
+	});
+
+    it('should display component', () => {
+        const logSpy = vi.spyOn(global.console, 'log');
+
+        render(<Component />);
+
+		expect(logSpy).toHaveBeenCalled();
+        expect(logSpy).toHaveBeenCalledWith('Hello from TestComponent')
+	});
+});
